fabea33110746ced392959822c1755f00aa3dbcb
gregmatic <60957555+gregmatic@users.noreply.github.com>
2025-08-28 01:33:20 +0800

    Update chopper_plot.py

--- chopper_plot.py
+++ chopper_plot.py
@@ -168,10 +168,10 @@ def main():
     # Graphs generation
     colors = ['', '#2F4F4F', '#12B57F', '#9DB512', '#DF8816', '#1297B5', '#5912B5', '#B51284', '#127D0C']
     plot_configs = [
-        (samples_median_adjusted, 'median_adjusted', 'Median Magnitude vs Parameters (Static Subtracted)'),
-        (samples_median_raw, 'median_raw', 'Median Magnitude vs Parameters (Raw Data)'),
-        (samples_avg_adjusted, 'avg_adjusted', 'Average Magnitude vs Parameters (Static Subtracted)'),
-        (samples_avg_raw, 'avg_raw', 'Average Magnitude vs Parameters (Raw Data)')
+        (samples_median_adjusted, 'median_adj_', 'Median Magnitude vs Parameters (Static Subtracted)'),
+        (samples_median_raw, 'median_raw_', 'Median Magnitude vs Parameters (Raw Data)'),
+        (samples_avg_adjusted, 'avg_adj_', 'Average Magnitude vs Parameters (Static Subtracted)'),
+        (samples_avg_raw, 'avg_raw_', 'Average Magnitude vs Parameters (Raw Data)')
     ]
     plot_paths = []
     
@@ -185,7 +185,8 @@ def main():
                 color = colors[toff if toff <= 8 else toff - 8]
                 fig.add_trace(go.Bar(x=[entry[1]], y=[entry[0]], marker_color=color, orientation='h', showlegend=False))
             fig.update_layout(title=title, xaxis_title='Magnitude', yaxis_title='Parameters', coloraxis_showscale=True)
-            plot_html_path = os.path.join(RESULTS_FOLDER, f'{name}{name_prefix}interactive_plot_{accel_chip}_tmc{driver}_{sense_resistor}_{now}.html')
+            #plot_html_path = os.path.join(RESULTS_FOLDER, f'{name}{name_prefix}interactive_plot_{accel_chip}_tmc{driver}_{sense_resistor}_{now}.html')
+            plot_html_path = os.path.join(RESULTS_FOLDER, f'{name}{name_prefix}_{now}.html')
             pio.write_html(fig, plot_html_path, auto_open=False)
             plot_paths.append(plot_html_path)
         
