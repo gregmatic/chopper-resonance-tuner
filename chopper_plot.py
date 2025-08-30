e10254f59238ecff60e63d3b9cd63d4d45b4c9fe
gregmatic <60957555+gregmatic@users.noreply.github.com>
2025-08-30 22:42:35 +0800

    Update chopper_plot.py

--- chopper_plot.py
+++ chopper_plot.py
@@ -111,7 +111,7 @@ def main():
     datapoint_avg_adjusted = []
     datapoint_avg_raw = []
     empty_error = 0
-    data_files = sorted(os.listdir(DATA_FOLDER), key=lambda x: os.path.getmtime(os.path.join(DATA_FOLDER, x)), reverse=False)
+    data_files = sorted(os.listdir(DATA_FOLDER), key=lambda x: os.path.getmtime(os.path.join(DATA_FOLDER, x)), reverse=True)
     # Filter for '__.csv' files
     csv_files = [name for name in data_files if name.endswith('__.csv')]
     total_files = len(csv_files)  # Count only '__.csv' files
